185999e16e3bb7214af7fa64d29ef302c0ec12a5
Spiewak <bspiewak6@gmail.com>
2020-09-06 09:35:00 -0400

    started localStorage save button function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 $("#currentDay").text(moment().format("llll"));
 var taskBlock = $("#text-area")
 var selectedHour = $(".selected-hour");
+var taskInput = JSON.parse(localStorage.getItem("taskItem")) || [];
 
 // function that changes background color as time passes throughout the calendar day
 var hourColor = function() {
@@ -28,14 +29,28 @@ var hourColor = function() {
 };
 hourColor();
 
-// save to local storage after entering a task and clicking the button
+// function when user clicks the save button
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
 
-// $(".saveBtn").on("click", function(){
-//     var userInput = taskBlock.value
-//     localStorage.setItem("taskItem", JSON.stringify(list));
+// Get the task "value" from the textbox and store it as a variable using `.val()` and `.trim()`
+    var taskValue = $('#text-area')
+    .val()
+    .trim();
 
-// };
+    var taskID = $(this)
+    .closest("div")
+    .text();
 
+    console.log(taskID);
 
+// add the new task to my new variable
+    taskInput.push( {
+        text: taskValue,
+        id: taskID
+    });
 
-// var userInput = JSON.parse(localStorage.getItem('taskItem')) || [];
+// save the tasks into localStorage
+    localStorage.setItem("taskItem", JSON.stringify(taskInput));
+});
+
