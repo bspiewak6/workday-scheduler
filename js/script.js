9761f3f13a436bd24c8662b5c41485af8535e2ec
Spiewak <bspiewak6@gmail.com>
2020-09-05 22:34:40 -0400

    started working on localStorage

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // Displays the current date and time
 $("#currentDay").text(moment().format("llll"));
-
+var taskBlock = $("#text-area")
 var selectedHour = $(".selected-hour");
 
-// function that changes background color as time passes
+// function that changes background color as time passes throughout the calendar day
 var hourColor = function() {
     var currentHour = (moment().hour());
+    //console.log(selectedHour.data().time);
 
     for (var i = 0; i < selectedHour.length; i++) {
         var task = $(selectedHour[i])
@@ -27,5 +28,14 @@ var hourColor = function() {
 };
 hourColor();
 
+// save to local storage after entering a task and clicking the button
+
+// $(".saveBtn").on("click", function(){
+//     var userInput = taskBlock.value
+//     localStorage.setItem("taskItem", JSON.stringify(list));
+
+// };
 
 
+
+// var userInput = JSON.parse(localStorage.getItem('taskItem')) || [];
